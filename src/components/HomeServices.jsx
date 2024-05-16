bf1b6a3b01fe53f9ff08a811fb1ea60b1ff52b35
masud <masudranaacb@gmail.com>
2024-05-16 15:09:36 +0600

    added search functionality is service name

--- src/components/HomeServices.jsx
+++ src/components/HomeServices.jsx
@@ -4,8 +4,18 @@ import { useState } from "react";
 const HomeServices = () => {
    
     const [acending,setAsending]=useState(true)
-    const services=useServices(acending)
-    
+    const [searcheText,setSearchText]=useState("")
+    const [value, setValue] = useState("");
+    const services=useServices(acending,value)
+    const handelSearche=e=>{
+        e.preventDefault()
+        const searchValue=e.target.searcheText.value
+        setSearchText(searchValue)
+    }
+    function updateInput(e) {  
+        setValue(e.target.value);
+    }  
+    console.log(value)
     return (
         <div className="container mx-auto mt-24 mb-10">
             
@@ -13,6 +23,12 @@ const HomeServices = () => {
                 <h2 className="text-xl font-bold text-red-500">Services</h2>
                 <h2 className="text-4xl font-bold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+            </div>
+            <div>
+                <form onSubmit={handelSearche}>
+                    <input value={value} onChange={updateInput} type="text" name="searcheText" id=""  className="border border-red-100"/>
+                    <button className="btn bg-red-500 text-white">Search Now</button>
+                </form>
             </div>
             <button onClick={()=>setAsending(!acending)} className="border border-1 bg-red-500 px-2 text-white">{acending? "Low to High":"High To Low"}</button>
             <div className="grid grid-cols-3 gap-10">
--- src/hooks/useServices.jsx
+++ src/hooks/useServices.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 
-const useServices = (acending) => {
+const useServices = (acending, searcheText) => {
     const [services, setServices] = useState([])
-    console.log(acending)
+    console.log(searcheText)
     useEffect(() => {
-        fetch(`http://localhost:5000/services?act=${acending?"acending":"decending"}`)
+        fetch(`http://localhost:5000/services?act=${acending ? "acending" : "decending"}&search=${searcheText}`)
             .then(res => res.json())
             .then(data => setServices(data))
-
-            
-    }, [acending])
+    }, [acending, searcheText])
     return services
 };
 
